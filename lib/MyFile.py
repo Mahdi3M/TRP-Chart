6197334e050391ad31cdcee4a989f4df5365a274
Md. Mahdi Mohtasim <70072135+Mahdi3M@users.noreply.github.com>
2023-09-24 00:41:24 +0600

    -Channel Info Table Complete -Analysis Form Created -Result Page Created

--- lib/MyFile.py
+++ lib/MyFile.py
@@ -12,15 +12,24 @@ class Files():
         workbook = openpyxl.load_workbook(self.file_name)
         worksheet = workbook.active
         
-        # Create groups according to channels...
-        groups = {}
+        # Create chart_groups according to channels...
+        chart_groups = {}
+        table_groups = {}
         for row in worksheet.iter_rows(min_row=2, values_only=True):
             if row[0] and row[1] and row[2] and row[3] and row[4]:
                 
                 # Initialize channel if not exist or Append row is exists...
-                if int(row[2]) not in groups:
-                    groups[int(row[2])] = []
+                if int(row[2]) not in chart_groups:
+                    chart_groups[int(row[2])] = []
+                    table_groups[int(row[2])] = [0.0, 0, float('inf'), float('-inf'), row[3], row[4]]
                     
-                groups[int(row[2])].append((row[0], row[1], row[3], row[4]))
+                chart_groups[int(row[2])].append((row[0], row[1], row[3], row[4]))
                 
-        return groups
+                table_groups[int(row[2])][0] += float(row[1])
+                table_groups[int(row[2])][1] += 1
+                table_groups[int(row[2])][2] = min(float(row[1]), table_groups[int(row[2])][2])
+                table_groups[int(row[2])][3] = max(float(row[1]), table_groups[int(row[2])][3])
+                table_groups[int(row[2])][4] = min(row[3], table_groups[int(row[2])][4])
+                table_groups[int(row[2])][5] = max(row[4], table_groups[int(row[2])][5])
+                
+        return {'chart_data': chart_groups, 'table_data': table_groups}
--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class CustomWindow(QMainWindow):
         
         self.chart = None
         self.data = None
+        self.resolution = None
         
         # Define Widgets...
         menu_open_button = self.actionOpen
@@ -27,12 +28,20 @@ class CustomWindow(QMainWindow):
         self.welcome_page= self.findChild(QWidget, 'welcomePage')
         self.chart_page= self.findChild(QWidget, 'chartPage')
         self.analysis_page= self.findChild(QWidget, 'analysisPage')
+        self.qa_page= self.findChild(QWidget, 'qaPage')
+        self.result_page= self.findChild(QWidget, 'resultPage')
+        
+        self.analysis_table= self.findChild(QTableWidget, 'analysisTable')
+        self.next_button= self.findChild(QPushButton, 'nextButton')
+        self.submit_button= self.findChild(QPushButton, 'submitButton')
         
         copyright_label = self.findChild(QLabel, 'copyright')
         
         # Edit widget UI...
         self.pages.setCurrentWidget(self.welcome_page)
-            # self.chart_frame.setStyleSheet("border: 2px solid black;")
+        
+        self.analysis_table.setColumnCount(7)
+        self.analysis_table.setShowGrid(True)
         
         copyright_label.setText(f"© Md. Mahdi Mohtasim  {datetime.now().year}")
         
@@ -41,11 +50,18 @@ class CustomWindow(QMainWindow):
         menu_chart_button.triggered.connect(self.drawCustomChart)
         menu_analysis_button.triggered.connect(self.showAnalysisPage)
         
+        self.next_button.clicked.connect(self.showQAPage)
+        self.submit_button.clicked.connect(self.showResultPage)
+        
         
     def resizeEvent(self, event):
         # This function will be called whenever the window is resized
-        new_size = event.size()
-        print(f"Window size changed to: {new_size.width()} x {new_size.height()}")
+        self.resolution = event.size()
+        # print(f"Window size changed to: {new_size.width()} x {new_size.height()}")        
+        if self.pages.currentIndex() in [1]:
+            self.drawCustomChart()
+        elif self.pages.currentIndex() in [2, 3]:
+            self.showAnalysisPage()
 
         
     def getDataFromFile(self):
@@ -53,11 +69,10 @@ class CustomWindow(QMainWindow):
             # Fetching data from the Excel file...
             file_name = QFileDialog.getOpenFileName(self, "Open File", "", "Excel Files (*.xlsx)")
             file = Files(file_name[0])
-            self.data= file.load_data_from_excel()
-            print("Data Fetched")
+            self.data = file.load_data_from_excel()
         except Exception:
             print("Error Fetching The File...")
-            return     
+            return
         
         if self.pages.currentIndex() in [1]:
             self.drawCustomChart()
@@ -82,7 +97,7 @@ class CustomWindow(QMainWindow):
             self.chart = CustomChart()
             chart_layout = self.findChild(QVBoxLayout, 'verticalLayout')
             chart_layout.addWidget(self.chart.canvas)
-            self.chart.plot_custom_chart(self.data)
+            self.chart.plot_custom_chart(self.data['chart_data'])
         except Exception:
             print("Error Drawing The File...")
             return
@@ -90,8 +105,47 @@ class CustomWindow(QMainWindow):
             
     def showAnalysisPage(self):
         self.pages.setCurrentWidget(self.analysis_page)
+        if not self.data:
+            self.getDataFromFile()
+            return
         
+        for col in range(7):
+            if col in [3, 4]:
+                self.analysis_table.setColumnWidth(col, max(self.resolution.width()//7, 160))
+            elif col in [5, 6]:
+                self.analysis_table.setColumnWidth(col, max(self.resolution.width()//7, 120))
+            else:
+                self.analysis_table.setColumnWidth(col, max((self.resolution.width()-175)//7, 100))
+        
+        # print(self.resolution.width())
+        row_count = len(self.data['table_data'])
+        self.analysis_table.setRowCount(row_count)
 
+        header = self.analysis_table.horizontalHeader()
+        for col in range(7):
+            header.setStyleSheet(
+                "QHeaderView::section {"
+                f"background-color: #d4d4d4;"
+                "}"
+            )
+            
+        for row, (key, values) in enumerate(self.data['table_data'].items()):
+            item = QTableWidgetItem("Channel " + str(key))
+            item.setTextAlignment(Qt.AlignCenter)
+            self.analysis_table.setItem(row, 0, item)
+            for col, value in enumerate(values):
+                item = QTableWidgetItem(str(value))
+                item.setTextAlignment(Qt.AlignCenter)
+                self.analysis_table.setItem(row, col+1, item)
+                
+                
+    def showQAPage(self):
+        self.pages.setCurrentWidget(self.qa_page)
+                
+                
+    def showResultPage(self):
+        self.pages.setCurrentWidget(self.result_page)
+        
 
 
 if __name__ == "__main__":
